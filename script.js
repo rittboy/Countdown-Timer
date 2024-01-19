93443f85575b5e474702016ee9d397de008cf1d5
John Ritter <john.ritter.8788@gmail.com>
2024-01-19 04:04:45 -0500

    Started JS for project, checked to make sure variables were displaying correctly

---
+++ script.js
@@ -0,0 +1,9 @@
+const currentDate = new Date();
+console.log(currentDate);
+
+const targetDate = new Date('2024-08-07T04:30:37');
+console.log(targetDate);
+
+const difference = targetDate - currentDate;
+
+console.log(difference);
